df7ddfa8fc36fe402f99b749c4a2e1bf96d89351
*abdul* <abdulrahimans1997@gmail.com>
2023-03-16 23:16:37 +0530

    nee

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const connectDatabase = require("./config/database");
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
 
 connectDatabase();
-app.listen(process.env.PORT, () => {
+app.listen(8000, () => {
   console.log(
     `server listen to the port:${process.env.PORT} in ${process.env.NODE_ENV}`
   );
